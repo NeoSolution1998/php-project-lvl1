04b19b9bff7016306973e3156c4126cf3ae1cdc9
neosolution <neosolution1998@gmail.com>
2021-10-14 18:25:52 +0300

    7 шаг, добавлена игра НОД

--- Makefile
+++ Makefile
@@ -13,3 +13,5 @@ brain-even:
 
 brain-calc:
 	./bin/brain-calc
+brain-gcd:
+	./bin/brain-gcd
---
+++ src/Gcd.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Src\Gcd;
+
+use function cli\line;
+use function cli\prompt;
+
+function gcd()
+{
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, {$name}!");
+    line('Find the greatest common divisor of given numbers.');
+
+    for ($i = 0; $i < 3; $i++) {
+        $randNum1 = rand(0, 100);
+        $randNum2 = rand(0, 100);
+        line("Question: {$randNum1} {$randNum2}");
+        $answer = prompt('Your answer');
+        $correctAnswer = checkgcd($randNum1, $randNum2);
+        if ($answer == $correctAnswer) {
+            line('Correct!');
+        } else {
+            line("'{$answer}' is wrong answer ;(. Correct answer was '{$correctAnswer}'");
+            line("Let's try again, {$name}!");
+            return false;
+        }
+    }
+    line("Congratulations, {$name}!");
+}
+
+function checkgcd($n, $m)
+{
+    if ($m > 0) {
+        return checkgcd($m, $n % $m);
+    } else {
+        return abs($n);
+    }
+}
