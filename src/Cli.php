86754473c92a7ae6c640e288f826914653008ff0
neosolution <neosolution1998@gmail.com>
2021-10-15 17:08:39 +0300

    Фикс файла Cli.php

--- src/Cli.php
+++ src/Cli.php
@@ -5,10 +5,9 @@ namespace Brain\Games\Cli;
   use function cli\line;
   use function cli\prompt;
 
-function welcom(): string
+function helloName(): void
 {
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
-    return $name;
 }
