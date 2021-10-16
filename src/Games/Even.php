545932bbc0294ce214a0b3bebfa36cbd539985e0
neosolution <neosolution1998@gmail.com>
2021-10-16 13:10:29 +0300

    Правки 1.04

--- src/Games/Even.php
+++ src/Games/Even.php
@@ -7,11 +7,13 @@ use function cli\prompt;
 use function Src\Engine\welcome;
 use function Src\Engine\engine;
 
+use const Src\Engine\ROUNDS_COUNT;
+
 function game(): void
 {
     $name = welcome();
     line('Answer "yes" if the number is even, otherwise answer "no".');
-    for ($i = 0; $i < 3; $i++) {
+    for ($i = 0; $i < ROUNDS_COUNT; $i++) {
         $randNum = rand(0, 100);
         $question = (string) $randNum;
         $correctAnswer = isEven($randNum);
