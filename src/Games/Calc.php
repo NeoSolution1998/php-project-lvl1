4bb171a13b224bde6c7233c6ba9e29f39e45a710
neosolution <neosolution1998@gmail.com>
2021-10-16 10:46:30 +0300

    Правки 1.01

--- src/Games/Calc.php
+++ src/Games/Calc.php
@@ -15,19 +15,18 @@ function calc(): void
     $name = welcome();
     line('What is the result of the expression?');
     for ($i = 0; $i < ROUNDS_COUNT; $i++) {
-        $randOperation = rand(0, 2);
-        $operation = $operations[$randOperation];
+        $randIndex = array_rand($operations, 1);
+        $randOperation = $operations[$randIndex];
         $randNum1 = rand(1, 20);
         $randNum2 = rand(1, 20);
-
-        if ($operation === '+') {
+        if ($randOperation === '+') {
             $correctAnswer = $randNum1 + $randNum2;
-        } elseif ($operation === '-') {
-            $correctAnswer === $randNum1 - $randNum2;
+        } elseif ($randOperation === '-') {
+            $correctAnswer = $randNum1 - $randNum2;
         } else {
             $correctAnswer = $randNum1 * $randNum2;
         }
-        $question = "{$randNum1} {$operation} {$randNum2}";
+        $question = "{$randNum1} {$randOperation} {$randNum2}";
         $correctAnswer = (string) $correctAnswer;
         $engine = engine($question, $correctAnswer);
         if ($engine) {
